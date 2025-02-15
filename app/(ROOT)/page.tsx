c167bcf1f9e494414322ef65e3c3463e4863596c
Michael Houmann <mhoumann87@gmail.com>
2025-02-15 10:42:26 +0100

    Add serarch form to home page

--- app/(ROOT)/page.tsx
+++ app/(ROOT)/page.tsx
@@ -1,5 +1,10 @@
+import SearchForm from "@/app/components/SearchForm";
 
-export default function Home() {
+export default async function Home(
+    { searchParams}: {searchParams: Promise<{query?: string}>}
+) {
+
+    const query = (await searchParams).query;
   return (
     <>
         <section className="pink_container">
@@ -12,6 +17,8 @@ export default function Home() {
             <p className="sub-heading !max-w-3xl">
                 Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
             </p>
+
+            <SearchForm query={query} />
         </section>
     </>
   );
